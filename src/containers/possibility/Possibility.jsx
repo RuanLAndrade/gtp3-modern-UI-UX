8d650efcf4533f856b94fd02e28b332f41bfe1c9
Ruan L. Andrade <ruanlandrade@outlook.com>
2022-03-09 13:49:23 -0300

    New Project

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.scss';
+
+const Possibility = () => {
+  return (
+    <div>
+      Possibility     
+    </div>
+  )
+}
+
+export default Possibility
---
+++ src/containers/whatIsGPT3/WhatIsGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatIsGPT3.scss';
+
+const WhatIsGPT3 = () => {
+  return (
+    <div>
+      WhatIsGPT3      
+    </div>
+  )
+}
+
+export default WhatIsGPT3;
